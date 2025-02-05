a1781137a9647e0003e872c1595a7283ad7a4f02
‪Mohamed Ragab <120748771+Mohamed-A-Ragab@users.noreply.github.com>
2025-02-05 18:51:51 +0200

    Update js.js

--- videos/js/js.js
+++ videos/js/js.js
@@ -1,20 +1,14 @@
 const apiKey = 'AIzaSyB8pIlLQ57AHqDF4yyYsl99squEpO11Xq4';
 const channelId = 'UC3NkYoeMjGkyPjk46TJFzHA';
 const maxResults = 10;
-
 const videoContainer = document.getElementById('video-list');
 
 async function fetchVideos() {
   try {
     const apiURL = `https://www.googleapis.com/youtube/v3/search?key=${apiKey}&channelId=${channelId}&part=snippet,id&type=video&order=date&maxResults=${maxResults}`;
-    console.log('Fetching from:', apiURL);
-    
     const response = await fetch(apiURL);
     const data = await response.json();
-    console.log('Response Data:', data);
-    
     videoContainer.innerHTML = '';
-    
     if (data.items) {
       data.items.forEach(item => {
         if (item.id && item.id.videoId) {
@@ -29,7 +23,6 @@ async function fetchVideos() {
       });
     }
   } catch (error) {
-    console.error('Error fetching videos:', error);
     videoContainer.innerHTML = '<p style="color:red;">Failed to load videos. Please try again later.</p>';
   }
 }
